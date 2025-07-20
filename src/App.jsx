941f723ccc6be658cb6257c8034cf9ea4d49b33a
kinjaldesarla <kinjaljain107@gmail.com>
2025-07-20 22:19:16 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Weather from './componenets/weather'
+import Weather from './componenets/Weather.jsx'
 
 
 
